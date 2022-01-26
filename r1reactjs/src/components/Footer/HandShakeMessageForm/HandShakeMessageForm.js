19e50688ee210ebb38df31b61c4f0d8068dd6f04
saitejsunkara9 <saitejsunkara9@gmail.com>
2022-01-26 13:08:33 +0530

    box shadows updated

--- r1reactjs/src/components/Footer/HandShakeMessageForm/HandShakeMessageForm.js
+++ r1reactjs/src/components/Footer/HandShakeMessageForm/HandShakeMessageForm.js
@@ -53,17 +53,17 @@ const HandShakeMessageForm = ({handShakeForm, setHandShakeForm, handleFormSubmit
         <form className="handShakeMessageForm" onSubmit={(e)=>handleFormSubmit(e)}>
             <div>
                 <label htmlFor="name" className='form-label'>Name:</label>
-                <input name="name" ref={autoFocusReference} type="text" required autoFocus value={handShakeForm.name} onChange={(e)=>handleHandShakeForm(e)} placeholder='Enter your name here' className='form-control' />
+                <input name="name" ref={autoFocusReference} type="text" required autoFocus value={handShakeForm.name} onChange={(e)=>handleHandShakeForm(e)} placeholder='Enter your name here' className='form-control' autoComplete='off' />
                 {formErrors.isNameError && isFormDirty.isNameFormDirty?<label className='form-text text-danger'>Name should be minimum of 2 characters and maximum of 30 characters.</label>:<label className='form-text'></label>}
             </div>
             <div>
                 <label htmlFor="email" className='form-label'>Email:</label>
-                <input name="email" type="email" required value={handShakeForm.email} onChange={(e)=>handleHandShakeForm(e)} placeholder='Enter your email here' className='form-control' />
+                <input name="email" type="email" required value={handShakeForm.email} onChange={(e)=>handleHandShakeForm(e)} placeholder='Enter your email here' className='form-control' autoComplete='off' />
                 {formErrors.isEmailError && isFormDirty.isEmailFormDirty?<label className='form-text text-danger'>Invalid email provided.</label>:<label className='form-text'></label>}
             </div>
             <div>
                 <label htmlFor="message" className='form-label'>Message:</label>
-                <textarea name="message" type="text" required value={handShakeForm.message} onChange={(e)=>handleHandShakeForm(e)} placeholder='Enter your message here' className='form-control' />
+                <textarea name="message" type="text" required value={handShakeForm.message} onChange={(e)=>handleHandShakeForm(e)} placeholder='Enter your message here' className='form-control' autoComplete='off' />
                 {formErrors.isMessageError && isFormDirty.isMessageFormDirty?<label className='form-text text-danger'>Message should be a minimum of 7 characters and maximum of 100 characters.</label>:<label className='form-text'></label>}
             </div>
             <div className='mt-3'>
